6a34e75bd187b2944c26094db752d6cec269f059
yuyan415 <ycheng316@gmail.com>
2024-03-08 16:37:56 -0500

    Update 2_filter_norm_dex.R

--- RNAseq/code/2_filter_norm_dex.R
+++ RNAseq/code/2_filter_norm_dex.R
@@ -3,11 +3,19 @@
 
 source("./code/00_utils.R")
 
-tfs <- c('ATF3','ATF4','CHOP','CEBPG')
 load('./output/filterEx.rda')
 outdir <- './output'
 
-tfs <- c('ATF3','ATF4','CHOP','CEBPG')
+# remove outliers identified from 1b_qc_outlier.R
+toDel <- c('ATF4.3d.6','ATF4.3d.7','ATF4.3d.5', 'CEBPG.3d.7', "non.3d.9","non.3d.11")
+idx <- is.na(match(rownames(Cov), toDel))
+Cov <- Cov[idx,]
+Ex <- Ex[, rownames(Cov)]
+
+COV <- cal_cov(Cov[,c(14:ncol(Cov))],outdir)
+Cov <- cbind(Cov, COV)
+
+save(Ex, Cov, geneAnno, file=paste0(outdir, '/filterEx.rda'))
 
 ##edgeR to filter lowly-expressed genes and normalize sequencing depth
 
