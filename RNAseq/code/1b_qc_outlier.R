c8cd7323a22866bcf4c98d49c995e4efbc8ea19b
yuyan415 <ycheng316@gmail.com>
2024-03-08 12:54:23 -0500

    Update 1b_qc_outlier.R

--- RNAseq/code/1b_qc_outlier.R
+++ RNAseq/code/1b_qc_outlier.R
@@ -1,24 +1,11 @@
 rm(list=ls())
 
-#load necessary packages
-library(edgeR)
-library(dplyr)
-library(fastDummies)
-library(Hmisc)
-library(corrplot)
-library(matrixStats)
-library(ggplot2)
-library(RColorBrewer)
-library(cowplot)
-library(ComplexHeatmap)
-library(circlize)
-
 
 source("./code/00_utils.R")
 
 tfs <- c('ATF3','ATF4','CHOP','CEBPG')
-load('./output/preprocess.rda')
-outdir <- './output'
+load('../output/preprocess.rda')
+outdir <- '../output'
 
 ## identify sample outliers
 # WGCNA sample connectivity absolute z score > 2
